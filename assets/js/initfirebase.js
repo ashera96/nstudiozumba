51b0b44e198668bb9c960db9d106ec2a2c669f28
Sachintha Madushan Rathnayake <madushansachintha@gmail.com>
2019-01-17 12:04:09 +0530

    Init firebase

---
+++ assets/js/initfirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyANibEP4Em0YoknfGPgYrGJ4xGpvVrKl9k",
+    authDomain: "n-studio-zumba.firebaseapp.com",
+    databaseURL: "https://n-studio-zumba.firebaseio.com",
+    projectId: "n-studio-zumba",
+    storageBucket: "n-studio-zumba.appspot.com",
+    messagingSenderId: "435999712713"
+};
+firebase.initializeApp(config);
